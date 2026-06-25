7f27ee27fc8987a9ff4855974db3eb591723d5dd
yugendra achary <yugendrachari09@gmailcom>
2026-06-25 11:59:09 +0530

    Day 2: Backend setup with Express, PostgreSQL and Prisma

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const indexRoutes = require("./routes");
+
+app.use("/", indexRoutes);
+
+module.exports = app;
